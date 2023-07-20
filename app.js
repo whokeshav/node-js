1d078904df24daf0996c152338c7a896fc153a63
whokeshav <94471056+whokeshav@users.noreply.github.com>
2023-07-20 09:20:25 +0530

    changes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const morgan=require('morgan');
 const mongoose=require('mongoose');
-const Blog=require('./models/blog');
+
+const blogRoutes=require('./routes/blogRoute');
 // express app
 const app = express();
 // connect to mongo db
@@ -21,45 +22,6 @@ app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
 
 
-//mongoose and mongo sandbox routes
-// app.get('/add-blog',(req,res)=>{
-//      const blog = new Blog({
-//         title:'new blog',
-//         snippet:'about my new blog',
-//         body:'more about my new blog '
-//      });
-//      blog.save()
-//          .then((result)=>{
-//             res.send(result)
-//          })
-//          .catch((err)=>{
-//             console.log(err);
-//          })
-// });
-// app.get('/all-blogs',(req,res)=>
-// {
-//     Blog.find()
-//     .then((result)=>
-//    {
-//     res.send(result);
-//    } )
-//    .catch((err)=>{
-//     console.log(err);
-//    })
-// })
-// app.get('/single-blog',(req,res)=>
-// {
-//     Blog.findById('64b62ddaeeeb2b5d8b9bac07')
-//       .then((result)=>
-//       {
-//         res.send(result)
-//       })
-//       .catch((err)=>{
-//         console.log(err);
-//        })
-
-// }
-// )
 
 
 
@@ -82,33 +44,10 @@ app.get('/about-us',(req,res)=>
     res.redirect('/about', {title:'about'});
 });
 // blog routes 
-app.get('/blogs',(req,res)=>{
-    Blog.find().sort({createdAt:-1})
-    .then((result)=>{
-          res.render('index', {title:'all blogs',blogs:result})
+app.use(blogRoutes());
 
 
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-})
-app.post('/blogs',(req,res)=>{
-    const blog= new Blog(req.body);
 
-    blog.save()
-    .then((result)=>{
-        res.redirect('/blogs');
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-
-})
-app.get('/blogs/create',(req,res)=>
-{
-    res.render('create', {title:'create'});
-})
 
 // 404 
 app.use((req,res)=> 
---
+++ routes/blogRoute.js
@@ -0,0 +1,64 @@
+const express=require('express');
+const router=expres.Router();
+const Blog=require('../models/blog');
+router.post('/', (req, res) => {
+    const blog= new Blog(req.body);
+  
+    blog.save()
+    .then(result=>
+      {
+          res.redirect('/blogs');
+      })
+      .catch((err)=>
+      {
+          console.log(err);
+      })
+  
+    });
+    router.get('/create',(req,res)=>
+  {
+      res.render('create', {title:'create blog'});
+  })
+    
+  router.get('/:id', (req, res) => {
+      const id = req.params.id;
+    
+      // Check if the id is a valid ObjectId
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).render('404', { title: 'Page Not Found' });
+      }
+    
+      Blog.findById(id)
+        .then((result) => {
+          if (result) {
+            res.render('details', { blog: result, title: 'Blog Details' });
+          } else {
+            res.status(404).render('404', { title: 'Blog Not Found' });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).render('error', { title: 'Internal Server Error' });
+        });
+    });
+    router.get('/',(req,res)=>{
+      Blog.find().sort({createdAt:-1})
+      .then((result)=>{
+            res.render('index', {title:'all blogs',blogs:result})
+  
+  
+      })
+      .catch((err)=>{
+          console.log(err);
+      })
+  })
+    
+  router.delete('/blogs/:id',(req,res)=>{
+      const id=req.params.id;
+      Blog.findByIdAndDelete(id)
+      .then((result)=>{
+          res.json({redirect:'/blogs'})
+      })
+      .catch(err=>{console.log(err);});
+  })
+  module.exports=router;
